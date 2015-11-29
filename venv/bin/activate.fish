e2eef3b4c8d5723c3e8b4a1015c5725f27c9995f
Duy Tan <ptdtan@gmail.com>
2015-11-30 02:46:18 +0700

    edit environmment file

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -32,7 +32,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ptdtan/rnavariantcalling/venv"
+set -gx VIRTUAL_ENV "/home/ptdtan/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
---
+++ venv/bin/active.py
@@ -0,0 +1,4 @@
+#! /usr/bin/env python
+
+activate_this = 'activate_this.py'
+execfile(activate_this, dict(__file__=activate_this))
