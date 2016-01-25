ac47ad2ccd2c7c26a1d5f0becf9a12eda9cc65f6
Pham Thai Hoa <thaihoabo@gmail.com>
2016-01-25 14:23:35 +0700

    Update code

---
+++ src/clean.py
@@ -0,0 +1,7 @@
+__author__ = 'linuxpham'
+import os
+
+if __name__ == '__main__':
+    ROOT_PATH="/home/hoabo/smartpipe/rnavariantcalling/HISAT2"
+    for path, subdirs, files in os.walk(ROOT_PATH):
+        print(subdirs)
