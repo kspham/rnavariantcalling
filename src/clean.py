aeeb6eefde4c9fac69afc154db1b35f8fb29ebcc
Pham Thai Hoa <thaihoabo@gmail.com>
2016-01-25 14:25:21 +0700

    Add clean BAM

--- src/clean.py
+++ src/clean.py
@@ -4,4 +4,5 @@ import os
 if __name__ == '__main__':
     ROOT_PATH="/home/hoabo/smartpipe/rnavariantcalling/HISAT2"
     for path, subdirs, files in os.walk(ROOT_PATH):
-        print(subdirs)
+        for subdir in subdirs:
+            print(subdir)
