70fe63a739243e02f61141917a3956755778cb52
ptdtan <ptdtan@gmail.com>
2016-05-21 00:35:55 +0700

    add firstheader

---
+++ scripts/firstheader.cpp
@@ -0,0 +1,23 @@
+//#include <boost/algorithm/string/predicate.hpp>
+//#include <boost/lexical_cast.hpp>
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main(){
+  bool header = true;
+  for (std::string line; std::getline(std::cin, line);){
+      if (line.substr(0, 2)=="##"){
+		if (header)
+        cout << line <<endl;
+        continue;
+      } else if (line.substr(0,1) == "#"){
+		 if (header){
+          cout << line << endl;
+          header = false;
+		 }
+          continue;
+	  }
+      cout << line << endl;
+  }
+}
